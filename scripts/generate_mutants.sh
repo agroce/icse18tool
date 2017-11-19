8002ebc7684f5f67abf5164f40663c1d6783f9c9
August Shi <awshi2@illinois.edu>
2017-11-19 16:00:59 -0600

    Fixing to get the original source in the same place, and making sure when checking if mutant compiles not to recompile old ones as well

--- scripts/generate_mutants.sh
+++ scripts/generate_mutants.sh
@@ -9,12 +9,14 @@ for f in $(find src/main/java/ -name "*.java"); do
     # Make relevant directories in both valid and invalid
     mkdir -p mutants/valid/$(dirname ${f})
     mkdir -p mutants/invalid/$(dirname ${f})
+
     # Generate all mutants into valid first
+    cp ${f} mutants/valid/$(dirname ${f})
     ${MUTATE} mutants/valid/${f} --noCheck --mutantDir mutants/valid/$(dirname ${f}) > /dev/null
 
     # For each mutant, copy it into the same place as the original, compile to see if it is valid
     # For each invalid mutant, move to relevant area
-    for m in $(find mutants/valid/ -name "*.mutant.*.java"); do
+    for m in $(find mutants/valid/ -name "$(basename ${f} | sed 's;.java;*.mutant.*.java;')"); do
         cp ${m} ${f}
         mvn clean compile > /dev/null   # Using clean is important, cannot trust Maven to compile properly without cleaning...
         if [[ $? != 0 ]]; then
