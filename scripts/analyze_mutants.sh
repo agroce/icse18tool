9342be55aecb0b182f52c0d047bf3e438c574e4e
August Shi <awshi2@illinois.edu>
2017-11-20 19:27:46 -0600

    Adding a timeout, saving results between files into a top-level file

--- scripts/analyze_mutants.sh
+++ scripts/analyze_mutants.sh
@@ -2,7 +2,17 @@
 
 ANALYZE=${HOME}/.local/bin/analyze_mutants
 
+# Set up top-level killed/not-killed
+killed=topkilled.txt
+notkilled=topnotkilled.txt
+rm -f ${killed}
+rm -f ${notkilled}
+
 # Try and analyze mutants with all valid mutants and running
 for f in $(find mutants/valid/ -name "*.java" | cut -d'.' -f1,4 | sort -u); do    # Use some cut magic to get the original file name
-    ${ANALYZE} $(echo ${f} | sed 's;mutants/valid/;;') --mutantDir $(dirname ${f}) "mvn clean test -Dsurefire.skipAfterFailureCount=1"
+    git checkout src/main/java
+    ${ANALYZE} $(echo ${f} | sed 's;mutants/valid/;;') --mutantDir $(dirname ${f}) "timeout 60s mvn clean test -Dsurefire.skipAfterFailureCount=1"
+    # Save results into top-level files
+    cat killed.txt >> ${killed}
+    cat notkilled.txt >> ${notkilled}
 done
